a96401cbeadf502c9504bb3cd3f58c47c7cb164f
Dhruv <dpipaliya159@gmail.com>
2025-12-23 14:45:57 +0530

    Practice Problem Page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import { Toaster } from "react-hot-toast";
 import ProblemsPage from "./pages/ProblemsPage";
+import ProblemPage from "./pages/ProblemPage";
 import { useUser } from "@clerk/clerk-react";
 import DashboardPage from "./pages/DashboardPage";
 
@@ -15,6 +16,7 @@ function App() {
       <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to="/dashboard"/>}/>
       <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to="/"/>}/>
       <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to="/"/>}/>
+      <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to="/"/>}/>
     </Routes>
     <Toaster /> 
     </>
